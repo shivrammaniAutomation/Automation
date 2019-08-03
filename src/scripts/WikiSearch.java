6046ee72dd85a8b622f9f6465b876387fc1c40a9
shivram <shivram@visiontemenos.com>
2019-08-03 17:58:07 -0400

    commit from user1

---
+++ src/scripts/WikiSearch.java
@@ -0,0 +1,19 @@
+package scripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WikiSearch {
+
+	public static WebDriver driver;
+	public static void main(String[] args) 
+	{
+	
+		System.setProperty("webdriver.chrome.driver", "src/resources/drivers/chromedriver");
+		 driver = new ChromeDriver();
+		 driver.get("https://www.wikipedia.org");
+		
+		
+	}
+
+}
