1670eacd5384559667232c1deb3ec10e4c783a25
shivram <shivram@visiontemenos.com>
2019-08-04 01:08:57 -0400

    new updates in Google master

--- src/scripts/GoogleTest.java
+++ src/scripts/GoogleTest.java
@@ -1,5 +1,6 @@
 package scripts;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -17,6 +18,10 @@ public class GoogleTest {
 			 driver = new ChromeDriver();
 			 driver.get("https://www.google.co.in");
 			 System.out.println("The website google has been opened");
+			 //type something in the Google search box
+			 driver.findElement(By.name("q")).sendKeys("Automation");
+			 //click on the Google Search button
+			 driver.findElement(By.name("btnK")).click();
 			 
 			
 			
